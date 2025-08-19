681acd6fd61e2e456faa4b1788c81e8ab7649dee
Hustle Airdrops <87428570+HustleAirdrops@users.noreply.github.com>
2025-08-19 20:40:34 +0530

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -88,7 +88,7 @@ rm -rf "$HOME/0g-storage-node/run/db/flow_db"
 
 echo "⬇️ Downloading and Extracting fast sync database..."
 
-wget https://github.com/HustleAirdrops/0G-Storage-Node/releases/download/Try/flow_db.tar.gz \
+wget https://snapshot.corenodehq.xyz/0g_testnet/flow_db.tar.gz \
   -O $HOME/0g-storage-node/run/db/flow_db.tar.gz && \
   tar -xzvf $HOME/0g-storage-node/run/db/flow_db.tar.gz -C $HOME/0g-storage-node/run/db/
   
